b54675d6a92017605fdeef1c3a017ad67f622aa3
NonkululekoNooi <nooi9423@gmail.com>
2022-07-12 13:02:32 +0200

    added

--- test/countAllFromTown.tests.js
+++ test/countAllFromTown.tests.js
@@ -3,7 +3,7 @@ let countAllFromTown = require("../countAllFromTown")
 
 describe ('Check how many registration number is from that town', function(){
 
-it("It should return '3' if the registration is called by the registration number",function(){
+it("It should return '3' if the registration is from 	Stellenbosch",function(){
 
  assert.deepEqual(3, countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL'))
 
@@ -12,7 +12,7 @@ it("It should return '3' if the registration is called by the registration numbe
 
 
 
-   it("It should return '0' if the registration is called by the registration number",function(){
+   it("It should return '0' if there is no registration from Cape Town ",function(){
 
     var fromCapeTown = countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341,CF')
 
